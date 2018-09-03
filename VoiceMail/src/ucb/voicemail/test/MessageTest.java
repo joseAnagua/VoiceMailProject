4582c012137f1ead758214681ef1a47a411ae113
Anagua <anh2o@hotmail.com>
2018-09-03 14:08:18 -0400

    Prueba para getText terminada

--- VoiceMail/src/ucb/voicemail/test/MessageTest.java
+++ VoiceMail/src/ucb/voicemail/test/MessageTest.java
@@ -2,13 +2,25 @@ package ucb.voicemail.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import ucb.voicemail.domain.Message;
 
 public class MessageTest {
 
+	Message message;
+	String text;
+	
+	@Before
+	public void init() {
+		text = "Hola soy mensaje de prueba";
+		message = new Message(text);
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void deberiaRetornarElMensaje() {
+		assertEquals(text, message.getText());
 	}
 
 }

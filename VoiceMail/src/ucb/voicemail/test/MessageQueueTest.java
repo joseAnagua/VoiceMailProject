f2db87bb451f3d063dcabc465c77404ccefd5710
Anagua <anh2o@hotmail.com>
2018-09-03 14:18:28 -0400

    Clase MessageQueueTest creada

---
+++ VoiceMail/src/ucb/voicemail/test/MessageQueueTest.java
@@ -0,0 +1,14 @@
+package ucb.voicemail.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MessageQueueTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

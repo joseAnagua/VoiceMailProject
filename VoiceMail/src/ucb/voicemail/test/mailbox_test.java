7d0660664642bfd18c065d35a00388e5d25fe7ba
Anagua <anh2o@hotmail.com>
2018-09-04 14:18:52 -0400

    Creando la clase mailbox test

---
+++ VoiceMail/src/ucb/voicemail/test/mailbox_test.java
@@ -0,0 +1,14 @@
+package ucb.voicemail.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class mailbox_test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

7e87b2a3a26820123ba64a52e2d137515d27c170
Anagua <anh2o@hotmail.com>
2018-09-11 00:38:04 -0400

    Prueba de voice mail que funcion la consola

--- VoiceMail/src/ucb/voicemail/test/TelephoneTest.java
+++ VoiceMail/src/ucb/voicemail/test/TelephoneTest.java
@@ -1,11 +1,17 @@
 package ucb.voicemail.test;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
-import org.junit.Before;
+
 import org.junit.Test;
+
 import com.sun.corba.se.pept.transport.Connection;
 
 import ucb.voicemail.domain.Telephone;
@@ -24,4 +30,26 @@ public class TelephoneTest {
 		verify(out).println("TestRuning");
 	}
 
+	/*@Test
+	public void deberiaDarElValorDeFalseAMore() {
+		String input = "Q";
+		InputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+		
+		this.telephone = new Telephone(new Scanner(System.in));
+		this.connection = mock(Connection.class);
+		this.telephone.run(connection);
+	}*/
+	
+	/*public void deberiaEjecutarElMetodoDialDeConnection() {		
+		String input = "1";
+	    InputStream in = new ByteArrayInputStream(input.getBytes());
+	    System.setIn(in);
+		
+		telephone = new Telephone(new Scanner(System.in));
+		
+		connection = mock(Connection.class);
+		
+		doNothing().when(connection).dial(isA(String.class));
+	}*/
 }

2adddfe5d251f349d230a77cb0e7c50434004780
joseAnagua <anah2o.jlaq@gmail.com>
2018-10-01 13:33:39 -0400

    Fusion de la rama de patron de disenio state

--- VoiceMail/src/ucb/voicemail/domain/Subject.java
+++ VoiceMail/src/ucb/voicemail/domain/Subject.java
@@ -1,7 +1,6 @@
 package ucb.voicemail.domain;
 
 public interface Subject {
-	
 	public void addTelephone(Telephone userInterface);
 	public void deleteTelephone(Telephone userInterface);
 	public void notifyToAll(String output);

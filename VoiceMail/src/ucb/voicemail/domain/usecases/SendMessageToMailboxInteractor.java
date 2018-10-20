1b4ce8566e8cce25087e6bb8184dba1c359f303f
joseAnagua <anah2o.jlaq@gmail.com>
2018-10-20 11:16:06 -0400

    Ultimos cambios de arquitectura limpia

---
+++ VoiceMail/src/ucb/voicemail/domain/usecases/SendMessageToMailboxInteractor.java
@@ -0,0 +1,22 @@
+package ucb.voicemail.domain.usecases;
+
+import ucb.voicemail.domain.MessageRepository;
+import ucb.voicemail.domain.boundary.input.SendMessageToMailboxUseCase;
+import ucb.voicemail.domain.boundary.output.Presenter;
+
+public class SendMessageToMailboxInteractor implements SendMessageToMailboxUseCase {
+
+	private MessageRepository repository;
+	private Presenter presenter;
+	
+	public SendMessageToMailboxInteractor(Presenter presenter, MessageRepository repository) {
+		this.presenter = presenter;
+		this.repository = repository;
+	}
+	
+	@Override
+	public void sendMessageToMailbox(String ext, String message) {
+		
+	}
+
+}
